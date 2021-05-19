cef1353e9424c58af98d000e860ff9e5356aa63d
linkitapps <master@linkit.kr>
2021-05-19 20:31:33 +0900

    인식 결과 보기 작업중

--- API/views.py
+++ API/views.py
@@ -1,5 +1,7 @@
 import base64
 import os
+import cv2
+import numpy as np
 from wsgiref.util import FileWrapper
 
 from django.core.files.base import ContentFile
@@ -61,13 +63,97 @@ def get_floor_plan_data_json(file_name, compartments):
     original_floor_plan_1 = convert_image_to_base64(tmp_folder_path + org_name)
     proc_name = "processed_" + file_name
     processed_floor_plan_1 = convert_image_to_base64(tmp_folder_path + proc_name)
+    
+
+
+
+
+    # yolo 1
+    img_filepath = tmp_folder_path + org_name
+    img = cv2.imread(img_filepath)
+    img_height, img_width, img_channels = img.shape
+    img_color = cv2.imread(img_filepath)
+    img_color = cv2.resize(img_color,(img_width,img_height))
+    img = cv2.resize(img,(img_width,img_height))
+    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img_gray = 255-img_gray   #inverted to highlight solid lines in white
+    gray = cv2.threshold(img_gray.copy(), 235,255, cv2.THRESH_BINARY)[1]
+
+    # Find the connected components on the floor
+    nb_components, labels, stats, centroids = cv2.connectedComponentsWithStats(gray, connectivity=8)# takes in gray image, connectivity to lookout for corners and output image data type.
+
+    #Threshold to filterout noises
+    gap_threshold = 20
+
+    #Create a empty list to collect identified objects
+    Walls_object=[]
+
+    unique = np.unique(labels) #labels are in multitude, so we get only unique labels
+
+    #Loop through the labels to highlight identified areas and gets stats(coordinates) f
+    for i in range(0, len(unique)):
+        component = labels == unique[i]  # enforce to select only unique label
+        stat = stats[i]    # get the stats and centroid for that label
+        centroid= centroids[i]
+        if img_color[component].sum() == 0 or np.count_nonzero(component) < gap_threshold:   #filter out the undesirable noises
+            color = 0
+        else:
+            Walls_object.append([i, component,stat, centroid])  # collect the labels and stats 
+            color = np.random.randint(0, 255, size=3)
+        img_color[component] = color    #hightlight label/components on image 
+
+    string1 = base64.b64encode(cv2.imencode('.jpg', img_color)[1]).decode()
+
+
+    # yolo 2
+    img_filepath = tmp_folder_path + org_name
+    img = cv2.imread(img_filepath)
+    img_height, img_width, img_channels = img.shape
+    img_color = cv2.imread(img_filepath)
+    img_color = cv2.resize(img_color,(img_width,img_height))
+    img = cv2.resize(img,(img_width,img_height))
+    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray = cv2.threshold(img_gray.copy(), 235,255, cv2.THRESH_BINARY)[1]
+
+    # Find the connected components on the floor
+    nb_components, labels, stats, centroids = cv2.connectedComponentsWithStats(gray, connectivity=8)# takes in gray image, connectivity to lookout for corners and output image data type.
+
+    #Threshold to filterout noises
+    gap_threshold = 20
+
+    #Create a empty list to collect identified objects
+    Walls_object=[]
+
+    unique = np.unique(labels) #labels are in multitude, so we get only unique labels
+
+    #Loop through the labels to highlight identified areas and gets stats(coordinates) f
+    for i in range(0, len(unique)):
+        component = labels == unique[i]  # enforce to select only unique label
+        stat = stats[i]    # get the stats and centroid for that label
+        centroid= centroids[i]
+        if img_color[component].sum() == 0 or np.count_nonzero(component) < gap_threshold:   #filter out the undesirable noises
+            color = 0
+        else:
+            Walls_object.append([i, component,stat, centroid])  # collect the labels and stats 
+            color = np.random.randint(0, 255, size=3)
+        img_color[component] = color    #hightlight label/components on image 
+
+    string2 = base64.b64encode(cv2.imencode('.jpg', img_color)[1]).decode()
+
+
+
+
+
     os.remove(tmp_folder_path + org_name)
     os.remove(tmp_folder_path + proc_name)
+
     return {
         "original_floor_plan_name": org_name,
         "original_floor_plan_image": original_floor_plan_1.decode(),
         "processed_floor_plan_name": proc_name,
         "processed_floor_plan_image": processed_floor_plan_1.decode(),
+        "processed_floor_plan_image_yolo1": string1,
+        "processed_floor_plan_image_yolo2": string2,
         "living_room": compartments.living_room,
         "hall": compartments.hall,
         "bathroom": compartments.bathroom,
